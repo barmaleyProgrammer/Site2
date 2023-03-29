a30c13a4d05f6bf447f872d00e0c8281e10606a4
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-03-29 22:37:03 +0300

    +s str12

--- 1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect('127.0.0.1', 'root', '', 'barmaley', 3306);
-
-$result = mysqli_query($conn, 'SELECT name FROM dict WHERE name LIKE \'%x\' ORDER BY name DESC;');
-
-while ($row = mysqli_fetch_assoc($result)) {
-    echo $row['name'].'<br>';
-}
